4634ad75370bc9a0de73abc2fb43c8926b75af4c
Pampamlela <pampamlela@gmail.com>
2025-02-28 10:17:05 +0100

    début du html

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+/*Les choses sérieuses commencent. 
+Il est temps d’interagir avec notre page en JavaScript !
+
+On passe donc sur notre ficher JS. Ajoutez un gestionnaire 
+d’événement avec addEventListener sur le formulaire, 
+n'hesitez pas à jeter un oeil à la documentation sur les forms ou les eventListener, 
+c'est le moment ;).
+
+Dans la fonction appelée lors de la soumission du formulaire :
+
+- Récupérez les valeurs des champs <input> dans deux variables : text et author.
+- Utilisez console.log() pour afficher ces valeurs et vérifier leur récupération.*/
